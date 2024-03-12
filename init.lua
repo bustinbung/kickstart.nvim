34ebbcf5bd09f23063568a89028a6676d19cb1c3
Justin Jung <justin@bustinbung.com>
2024-03-12 11:13:44 -0400

    Fix merge conflicts

--- init.lua
+++ init.lua
@@ -409,7 +409,13 @@ require('lazy').setup({
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', opts = {
+        notification = {
+          window = {
+            winblend = 0,
+          },
+        },
+      } },
     },
     config = function()
       -- Brief Aside: **What is LSP?**
@@ -723,9 +729,29 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'catppuccin/nvim',
-    name = 'catppuccin',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    init = function()
+      require('catppuccin').setup {
+        flavor = 'mocha',
+        integrations = {
+          fidget = true,
+          mason = true,
+          mini = {
+            enabled = true,
+            indentscope_color = 'sapphire',
+          },
+          which_key = true,
+        },
+      }
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'catppuccin'
+
+      -- You can configure highlights by doing something like
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
   },
 
   -- Highlight todo, notes, etc in comments
