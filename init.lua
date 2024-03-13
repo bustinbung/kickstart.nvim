db9f5db777a858c196f7ab6607bd43c9bec4b00d
Justin Jung <justin@bustinbung.com>
2024-03-12 20:54:15 -0400

    Add neorg plugin

--- init.lua
+++ init.lua
@@ -833,6 +833,29 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
 
+  {
+    'nvim-neorg/neorg',
+    build = ':Neorg sync-parsers',
+    lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
+    -- tag = "*",
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {}, -- Loads default behaviour
+          ['core.concealer'] = {}, -- Adds pretty icons to your documents
+          ['core.dirman'] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = '~/Nextcloud/neorg',
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
