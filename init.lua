a82c43cf6838cb07f197ee3f40f85781e259abd2
Justin Jung <justin@bustinbung.com>
2024-04-02 22:13:04 -0400

    Update neorg install to match v8 spec

--- init.lua
+++ init.lua
@@ -863,11 +863,14 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
 
   {
+    'vhyrro/luarocks.nvim',
+    priority = 1000,
+    config = true,
+  },
+  {
     'nvim-neorg/neorg',
-    build = ':Neorg sync-parsers',
     lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
-    -- tag = "*",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'luarocks.nvim' },
     config = function()
       require('neorg').setup {
         load = {
