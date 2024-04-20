421a95715384026a21001681819388ccedd86bc6
Justin Jung <justin@bustinbung.com>
2024-04-20 17:29:54 -0400

    Change to light mode

--- init.lua
+++ init.lua
@@ -773,7 +773,7 @@ require('lazy').setup({
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
       require('catppuccin').setup {
-        flavor = 'mocha',
+        flavor = 'auto',
         integrations = {
           fidget = true,
           mason = true,
@@ -787,8 +787,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
-
+      vim.cmd.colorscheme 'catppuccin-latte'
       -- You can configure highlights by doing something like
       -- vim.cmd.hi 'Comment gui=none'
     end,
